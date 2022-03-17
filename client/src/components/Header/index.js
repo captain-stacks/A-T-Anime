4118d4b5aa97a8e7facc686fcb48ffe29a7bbc3a
Connor Hollis <connorhollis3d@gmail.com>
2022-03-17 16:14:12 -0600

    added anime card component/ page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -14,7 +14,7 @@ const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">ATAnime</Link>
+        <h1><Link to="/">ATAnime</Link></h1>
         <Link to="/">Home</Link>
         <Link to="/anime">Anime</Link>
         <Link to="/your-list">Your List</Link>
