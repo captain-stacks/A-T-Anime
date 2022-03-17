9f99aa4e5c91693a62dfa9d9970de99a4ecd3bf2
Shane McElrath <shane@rangermac.com>
2022-03-17 15:20:24 -0400

    Added though authenticaiton err

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,7 +15,7 @@ const resolvers = {
         return userData;
       }
     
-      //throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError('Not logged in');
     },
     // get all users
     users: async () => {
