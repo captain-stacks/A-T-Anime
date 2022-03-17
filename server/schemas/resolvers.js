49d615874d57ffeceac88fcaa54b09126ffa21f4
Shane McElrath <shane@rangermac.com>
2022-03-17 13:04:57 -0400

    Finish schemas file set up

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,22 @@
+const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+//const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('thoughts')
+          .populate('friends');
+    
+        return userData;
+      }
+    
+      throw new AuthenticationError('Not logged in');
+    },
+  }
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Query {
+    me: User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
