df055b2e2b4716016ea2a34bc853f9def40b1ae8
Ella <ellacodes2021@gmail.com>
2022-03-27 15:50:29 -0400

    updated btns to correct sizing now

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -65,8 +65,8 @@ const Login = (props) => {
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className="btn d-block w-100" type="submit">
-                Log In
+              <button className="btn waves-effect waves-light" type="submit">
+                Log In <i class="material-icons right">send</i>
               </button>
             </form>
             <p className='pLogin-Signup'>
