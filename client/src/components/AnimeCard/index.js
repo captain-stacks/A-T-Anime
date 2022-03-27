f8ef006a5c09b5767e1eb95f2a0f7bdee6cf0f90
Connor Hollis <connorhollis3d@gmail.com>
2022-03-27 10:59:01 -0600

    moved the title of anime and button to bottom of card and center aligned title with Ella and Shane and Connor

--- client/src/components/AnimeCard/index.js
+++ client/src/components/AnimeCard/index.js
@@ -7,18 +7,18 @@ export default function AnimeCard(props) {
     return (
         <li className="btn-anime col s12 m6 l3 ">
             <div className="card">
-                <div className="collapsible">
-                    <AddToMyList animeId={props.animeId} favorite={props.favorite} />
-                </div>
                 <div className="card-image favAnimeImg">
+                    <img className="responsive-imgfavAnimeImg anime-card-height" alt="" src={props.image} />
                     <a href={props.image} className="favAnimeContainer">
                         <h3 className="">
-                            <span className="">
+                            <span className="center-align">
                                 {props.title}
                             </span>
                         </h3>
-                        <img className="responsive-imgfavAnimeImg anime-card-height" alt="" src={props.image} />
                     </a>
+                </div>
+                <div className="collapsible">
+                    <AddToMyList animeId={props.animeId} favorite={props.favorite} />
                 </div>
             </div>
         </li>
