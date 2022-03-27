beeb8cfded41a08b4c70ab606acbd41249889a87
Connor Hollis <connorhollis3d@gmail.com>
2022-03-27 16:28:55 -0600

    added underline on nav headers with Shane and Ella and Connor

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Home from '../../pages/Home';
 import Login from '../../pages/Login';
@@ -11,12 +11,14 @@ const Header = () => {
     Auth.logout();
   };
 
+  const[seletedSection, setSelectedSection] = useState("Home");
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1><Link to="/">ATAnime</Link></h1>
-        <Link to="/">Home</Link>
-        <Link to="/anime">Anime</Link>
+        <h1><Link onClick={() => setSelectedSection("Home")} to="/">ATAnime</Link></h1>
+        <Link onClick={() => setSelectedSection("Home")} className={seletedSection === "Home" && "headerSelected"} to="/">Home</Link>
+        <Link onClick={() => setSelectedSection("Anime")} className={seletedSection === "Anime" && "headerSelected"} to="/anime">Anime</Link>
 
         <nav className="text-center">
           {Auth.loggedIn() ? (
