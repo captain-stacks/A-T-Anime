a18269045faa6b55727ccb6526b5a15e10b66d61
Ella <ellacodes2021@gmail.com>
2022-03-28 20:27:13 -0400

    media querries set for nav tab

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -15,7 +15,7 @@ const Header = () => {
 
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+      <div className="container flex-row flex justify-space-between-lg justify-center align-center">
         <h1><Link onClick={() => setSelectedSection("Home")} to="/">ATAnime</Link></h1>
         <Link onClick={() => setSelectedSection("Home")} className={(seletedSection === "Home") ? ("headerSelected") : ('')} to="/">Home</Link>
         <Link onClick={() => setSelectedSection("Anime")} className={(seletedSection === "Anime") ? ("headerSelected") : ('')} to="/anime">Anime</Link>
