179ad8313b484fa6fea131694a17b4497b7847a2
Connor Hollis <connorhollis3d@gmail.com>
2022-03-28 18:04:36 -0600

    added google fonts with the whole team

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -16,21 +16,21 @@ const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1><Link onClick={() => setSelectedSection("Home")} to="/">ATAnime</Link></h1>
-        <Link onClick={() => setSelectedSection("Home")} className={(seletedSection === "Home") ? ("headerSelected") : ('')} to="/">Home</Link>
-        <Link onClick={() => setSelectedSection("Anime")} className={(seletedSection === "Anime") ? ("headerSelected") : ('')} to="/anime">Anime</Link>
+        <h1><Link onClick={() => setSelectedSection("Home")} className="fontWebsite" to="/">AtAnime</Link></h1>
+        <Link onClick={() => setSelectedSection("Home")} className={(seletedSection === "Home") ? ("headerSelected") : ('')} id="fontWebsiteNav"  to="/">Home</Link>
+        <Link onClick={() => setSelectedSection("Anime")} className={(seletedSection === "Anime") ? ("headerSelected") : ('')} id="fontWebsiteNav" to="/anime">Anime</Link>
 
         <nav className="text-center roundedButton">
           {Auth.loggedIn() ? (
             <>
-              <Link onClick={() => setSelectedSection("animeList")} className={(seletedSection === "animeList") ? ("personalSelected") : ('')} to="/profile">My Anime List</Link>
-              <a href="/" onClick={logout}>
+              <Link onClick={() => setSelectedSection("animeList")} className={(seletedSection === "animeList") ? ("personalSelected") : ('')} id="fontWebsiteNav" to="/profile">My Anime List</Link>
+              <a href="/" id="fontWebsiteNav" onClick={logout}>
                 Logout
               </a>
             </>
           ) : (
             <>
-              <Link onClick={() => setSelectedSection("Login")} className={(seletedSection === "Login") ? ("personalSelected") : ('')} to="/login" >Login</Link>
+              <Link onClick={() => setSelectedSection("Login")} className={(seletedSection === "Login") ? ("personalSelected") : ('')} to="/login" id="fontWebsiteNav" >Login</Link>
             </>
           )}
         </nav>
